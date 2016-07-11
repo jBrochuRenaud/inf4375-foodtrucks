40034fdc03eadb84ad4341949d0852bda5e99f42
jBrochuRenaud <julienbrochurenaud@gmail.com>
2016-07-10 22:44:53 -0400

    Removed missing example classes references

--- src/main/java/ca/uqam/projet/controllers/ApplicationController.java
+++ src/main/java/ca/uqam/projet/controllers/ApplicationController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class ApplicationController {
 
-  @Autowired CitationRepository repository;
+  @Autowired FoodTruckRepository repository;
 
   @RequestMapping("/")
   public String index(Model model) {
-    model.addAttribute("citations", repository.findAll());
+    //model.addAttribute("foodtrucks", repository.findAll());
     return "index";
   }
 }
